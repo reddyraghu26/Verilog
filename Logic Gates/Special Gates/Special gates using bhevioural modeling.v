13618fab8b5a28366ab2ed6400f65edad8301f7d
reddyraghu26 <reddyraghu9640@gmail.com>
2026-02-24 22:32:05 +0530

    Added new file

---
+++ Logic Gates/Special Gates/Special gates using bhevioural modeling.v
@@ -0,0 +1,20 @@
+//Implementing special gates using bhevioural modeling
+//In this modeling we procedural block to describe how the hardware behaves.
+module special_gates(
+  input a,b,
+  output reg x,y  
+);
+  
+  //'*' means the block executes whenever any signal used on the RHS inside this always block changes 
+  always @(*) begin
+    x= a^b;       //NAND gate
+    y=~(a^b);     //NOR  gate
+        
+  end
+  //x and y shoulb be reg, because they are assigned in an always block
+  
+  
+endmodule
+    
+    
+  
