70db195c2b62aadc9854c88d0206c8c310a1aa54
reddyraghu26 <reddyraghu9640@gmail.com>
2026-03-06 17:37:02 +0530

    Updated

--- Combinational Circuits/Comparator/comparator.v
+++ Combinational Circuits/Comparator/comparator.v
@@ -1,3 +1,6 @@
+// Parameterized comparator to compare two inputs of configurable width
+// Compares A and B and generates equal, greater, and less signals
+// Width parameter allows the comparator size to be easily changed
 module comparator #(parameter width=2)(
   input [width-1:0]A,B,
   output eql,gret,less
